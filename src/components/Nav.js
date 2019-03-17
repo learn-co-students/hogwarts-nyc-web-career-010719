748fbb37e598af6402d45263062c6547c3e1898d
github username <github email address>
2019-03-17 15:59:04 -0400

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,8 @@
 import piggy from '../porco.png'
 import React from 'react'
+import Search from './Search'
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
@@ -11,6 +12,7 @@ const Nav = () => {
 				</a>
 			</div>
 			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<Search filter={props.filter}/>
 		</div>
 	)
 }
